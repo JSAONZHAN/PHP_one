aba0f68b3862f00a6b370621cc06a535739ae24f
LL <LL>
2016-11-25 18:28:58 +0800

    first commit

---
+++ zhanlei.php
@@ -0,0 +1,7 @@
+<?php
+echo <<<EOT
+	<div style="width:100px;height:100px;background-color:green;"></div>
+	<div style="width:100px;height:100px;background-color:red;"></div>
+	<div style="width:100px;height:100px;background-color:blue;"></div>
+EOT;
+?>
